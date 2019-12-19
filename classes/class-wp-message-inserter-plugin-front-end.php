92aecbc68bc9455bd8ccace71c7e941e2a36c5f4
Jonathan Stegall <jstegall@minnpost.com>
2019-12-19 16:25:35 -0600

    missed a couple non formatting changes

--- classes/class-wp-message-inserter-plugin-front-end.php
+++ classes/class-wp-message-inserter-plugin-front-end.php
@@ -113,8 +113,8 @@ class WP_Message_Inserter_Plugin_Front_End {
 				$conditional  = isset( $message_meta['conditional_group_id'][0] ) ? $message_meta['conditional_group_id'][0] : '';
 				$conditional  = maybe_unserialize( $conditional );
 
-				$conditional_result =  $conditional[0]['_wp_inserted_message_conditional_result'];
-				$conditional        =  $conditional[0]['_wp_inserted_message_conditional'];
+				$conditional_result = $conditional[0]['_wp_inserted_message_conditional_result'];
+				$conditional        = $conditional[0]['_wp_inserted_message_conditional'];
 
 				// If our key is equal to a conditional with a method?
 				$key = array_search( $conditional, array_column( $conditionals, 'name' ), true );
