484a96695a49704e3b3cc7522cc8923dea9f2cf9
Jonathan Stegall <jstegall@minnpost.com>
2020-01-23 13:22:54 -0600

    lint the front end script

--- assets/js/src/front-end/01-cookies.js
+++ assets/js/src/front-end/01-cookies.js
@@ -1,97 +1,104 @@
-// Sets Cookies
-function setCookie(name, value, days) {
-	var d = new Date();
-	d.setTime(d.getTime() + 24 * 60 * 60 * 1000 * days);
-	document.cookie = name + "=" + value + ";path=/;expires=" + d.toGMTString();
+/**
+ * Sets cookies
+ *
+ * @param {string} name
+ * @param {string} value
+ * @param {number} days
+ */
+function setCookie( name, value, days ) {
+	const d = new Date();
+	d.setTime( d.getTime() + 24 * 60 * 60 * 1000 * days );
+	document.cookie = name + '=' + value + ';path=/;expires=' + d.toGMTString();
 }
 
-// Reads Cookies
-function getCookie(name) {
-	var v = document.cookie.match("(^|;) ?" + name + "=([^;]*)(;|$)");
-	return v ? v[2] : null;
+/**
+ * Reads cookies
+ *
+ * @param {string} name
+ */
+function getCookie( name ) {
+	const v = document.cookie.match( '(^|;) ?' + name + '=([^;]*)(;|$)' );
+	return v ? v[ 2 ] : null;
 }
 
 // Faux "Session" checking/setting
 // Timestamp
-var currentcount = getCookie("count");
-var timestamp = Math.floor(new Date().getTime() / 1000);
-if (!getCookie("count")) {
+let currentCount = getCookie( 'count' );
+const timestamp = Math.floor( new Date().getTime() / 1000 );
+if ( ! getCookie( 'count' ) ) {
 	// First Visit - set count to 1
-	setCookie("count", 1, 365);
+	setCookie( 'count', 1, 365 );
 
 	// Set a timecheck cookie for an hour from now
-	setCookie("timecheck", timestamp + 3600, 365);
-} else {
-	if (timestamp > getCookie("timecheck")) {
-		// Update Timecheck to new value
-		setCookie("timecheck", timestamp + 3600, 365);
+	setCookie( 'timecheck', timestamp + 3600, 365 );
+} else if ( timestamp > getCookie( 'timecheck' ) ) {
+	// Update Timecheck to new value
+	setCookie( 'timecheck', timestamp + 3600, 365 );
 
-		// Count exists already and it has been an hour. Update count
-		setCookie("count", ++currentcount, 365);
-	}
+	// Count exists already and it has been an hour. Update count
+	setCookie( 'count', ++currentCount, 365 );
 }
 
-$(document).ready(function() {
+/**
+ * When jQuery is loaded, set up cookies and popups
+ *
+ */
+$( document ).ready( function() {
 	// Get our value for days to set cookie
-	var closetimedays = parseInt($(".closetimedays").val());
+	const closeTimeDays = parseInt( $( '.closetimedays' ).val() );
 
 	// Get our value for hours and divide by 24 to get proper percent of a day
-	var closetimehours = $(".closetimehours").val() / 24;
+	const closeTimeHours = $( '.closetimehours' ).val() / 24;
 
 	// Our Total for when the cookie should expire and show the banner again
-	var cookiedaytotal = closetimedays + closetimehours;
+	const cookieDayTotal = closeTimeDays + closeTimeHours;
 
 	// Check if we should be showing the banner
 	if (
-		$(".pop-banner").length &&
-		"true" !== getCookie("sm-closed") &&
-		!$(".pop-banner").hasClass("check-session-banner")
+		$( '.pop-banner' ).length &&
+		'true' !== getCookie( 'sm-closed' ) &&
+		! $( '.pop-banner' ).hasClass( 'check-session-banner' )
 	) {
-		$(".pop-banner").addClass("d-block");
+		$( '.pop-banner' ).addClass( 'd-block' );
 	}
 
 	// Popup Banner Close Button
-	$(".sm-close-btn").on("click", function(e) {
+	$( '.sm-close-btn' ).on( 'click', function( e ) {
 		e.preventDefault();
-		setCookie("sm-closed", true, cookiedaytotal);
-		$(".pop-banner").hide();
-	});
+		setCookie( 'sm-closed', true, cookieDayTotal );
+		$( '.pop-banner' ).hide();
+	} );
 
 	// Session Validating and showing proper banner
-	var operators = {
-		gt: function(a, b) {
+	const operators = {
+		gt( a, b ) {
 			return a >= b;
 		},
-		lt: function(a, b) {
+		lt( a, b ) {
 			return a <= b;
-		}
+		},
 	};
 
-	var urlParams = new URLSearchParams(window.location.search);
-	if (urlParams.get("count") !== null) {
-		currentcount = parseInt(urlParams.get("count"));
+	const urlParams = new URLSearchParams( window.location.search );
+	if ( urlParams.get( 'count' ) !== null ) {
+		currentCount = parseInt( urlParams.get( 'count' ) );
 	}
 
-	$(".check-session-banner").each(function() {
-		var banner_session_count = $(this)
-			.find(".session_count_to_check")
-			.val();
-
-		var banner_session_operator = $(this)
-			.find(".session_count_operator")
-			.val();
+	$( '.check-session-banner' ).each( function() {
+		const bannerSessionCount = $( this ).find( '.session_count_to_check' ).val();
+		const bannerSessionOperator = $( this ).find( '.session_count_operator' ).val();
 
 		if (
-			operators[banner_session_operator](
-				currentcount,
-				parseInt(banner_session_count)
+			operators[ bannerSessionOperator ](
+				currentCount,
+				parseInt( bannerSessionCount )
 			)
 		) {
-			if (!$(this).hasClass("pop-banner")) {
-				$(this).addClass("validated");
+			if ( ! $( this ).hasClass( 'pop-banner' ) ) {
+				$( this ).addClass( 'validated' );
 			} else {
-				!getCookie("sm-closed") ? $(this).addClass("validated") : "";
+				! getCookie( 'sm-closed' ) ? $( this ).addClass( 'validated' ) : '';
 			}
 		}
-	});
-});
+	} );
+} );
