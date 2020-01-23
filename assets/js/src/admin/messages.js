b07b7b2a4e48c448da3bdb181f4922f150e5c815
Jonathan Stegall <jstegall@minnpost.com>
2020-01-23 11:13:16 -0600

    add admin script comments

--- assets/js/src/admin/messages.js
+++ assets/js/src/admin/messages.js
@@ -1,3 +1,7 @@
+/**
+ * Show fields associated with the selected message type
+ * @param {string} selector the div that holds the radio fields
+ */
 function showTypeField( selector ) {
 	var value  = $( 'input[type="radio"]:checked', selector ).val();
 	var parent = selector.parent();
@@ -5,6 +9,11 @@ function showTypeField( selector ) {
 	$( '.cmb2-message-type-' + value ).show();
 }
 
+/**
+ * Whether we need the maximum screen width text field or not
+ * @param {string} selector the div that holds the checkboxes
+ * @param {string} current the currently checked item
+ */
 function showHideMaximumScreen( selector, current ) {
 	if ( 'undefined' === typeof current ) {
 		var current = $( 'input[type="checkbox"]:checked', selector );
@@ -16,6 +25,10 @@ function showHideMaximumScreen( selector, current ) {
 	}
 }
 
+/**
+ * Whether we need the custom maximum banner width fields
+ * @param {string} value the value of the maximum width <select> field
+ */
 function showHideMaximumBanner( value ) {
 	if ( 'custom' === value ) {
 		$( '.cmb2-custom-maximum-banner-width' ).show();
@@ -24,6 +37,9 @@ function showHideMaximumBanner( value ) {
 	}
 }
 
+/**
+ * Set up the message admin fields. This runs when a "screen size" gets added to the form
+ */
 function setupMessage() {
 	var type_selector             = $( '.cmb2-message-type-selector' );
 	var no_max_screen_selector    = $( '.cmb2-no-maximum-screen-width' );
@@ -48,17 +64,26 @@ function setupMessage() {
 	}
 }
 
-$( document ).ready( function() {
-		$( '#pageparentdiv label[for=parent_id]' ).parents( 'p' ).eq(0).remove();
-		$( '#pageparentdiv select#parent_id' ).remove();
-	setupMessage();
-});
-
+/**
+ * When "add another screen size" runs, it adds a CMB2 row to the form. Set up the message form.
+ */
 $( document ).on( 'cmb2_add_row', function( e ) {
 	$( this ).find( 'input[type="checkbox"]' ).prop( 'checked', false );
 	setupMessage();
 });
 
+/**
+ * When jQuery loads, remove some default WP fields and set up the message form
+ */
+$( document ).ready( function() {
+	$( '#pageparentdiv label[for=parent_id]' ).parents( 'p' ).eq(0).remove();
+	$( '#pageparentdiv select#parent_id' ).remove();
+	setupMessage();
+});
+
+/**
+ * Make the various <select> fields into select2 fields
+ */
 if ( jQuery.fn.select2 ) {
 	$( '.cmb2-insertable-message select' ).select2();
 
